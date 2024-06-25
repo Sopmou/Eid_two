afb76ca77d0f968a180414d16f08b86e5bf1ab7c
Sopmou <sophjanmoubarrid@gmail.com>
2024-06-25 09:11:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
-let availableLambs = 10; // Beispiel für verfügbare Lämmer
 
 app.use(bodyParser.json());
 
+function readData() {
+    const rawData = fs.readFileSync('data.json');
+    return JSON.parse(rawData);
+}
+
+function writeData(data) {
+    fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+}
+
+// Endpunkt: Anzahl der verfügbaren Lämmer abrufen
 app.get('/api/lamb-count', (req, res) => {
-    res.json({ count: availableLambs });
+    const data = readData();
+    res.json({ count: data.availableLambs });
 });
 
+// Endpunkt: Verfügbare Abholdaten abrufen
+app.get('/api/pickup-dates', (req, res) => {
+    const data = readData();
+    res.json({ dates: data.pickupDates });
+});
+
+// Endpunkt: Lamm-Bestellung aufgeben
 app.post('/api/order-lamb', (req, res) => {
     const { firstName, lastName, pickupPerson, pickupDate } = req.body;
-    
-    if (availableLambs > 0) {
-        availableLambs--;
-        // Hier kannst du die Bestellung in einer Datenbank speichern
+    const data = readData();
+
+    if (data.availableLambs > 0) {
+        data.availableLambs--;
+        writeData(data);
         res.status(200).json({ message: 'Bestellung erfolgreich' });
     } else {
         res.status(400).json({ message: 'Keine Lämmer mehr verfügbar' });
